6c4b2f370544e2702fcfdadf7682231bedd9aeac
Michael Moradi <themoradis1@gmail.com>
2021-06-02 18:23:30 -0400

    User_Authentication

---
+++ db/migrate/20210602205642_drop_email_and_password_digest_colums_from_users.rb
@@ -0,0 +1,6 @@
+class DropEmailAndPasswordDigestColumsFromUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :email
+    remove_column :users, :password_digest
+  end
+end
